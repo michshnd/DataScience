cfcd1c190ef8f3932fdaa5aaaa215cd43fb90ed4
“michshnd” <michshnd@gmail.com>
2019-11-30 12:34:38 +0200

    HW R

--- R/.ipynb_checkpoints/R-final-checkpoint.R
+++ R/.ipynb_checkpoints/R-final-checkpoint.R
@@ -1,21 +1,16 @@
 
+#### On Linux server:
+library(DBI)
+con <- dbConnect(odbc::odbc(), .connection_string = "Driver={ODBC Driver 17 for SQL Server};server=192.168.1.1;
+database=COLLEGE;uid=dsuser02;
+pwd=DSuser02!", timeout = 10)
+
 #### On Windows:
 library(DBI)
 con <- dbConnect(odbc::odbc(), .connection_string = "DSN=COLLEGE;Trusted_Connection=yes;", timeout = 10)
 
 ## Get the whole table:
-classroom <- dbReadTable(con, "Classrooms")
-teachers <- dbReadTable(con, "Teachers")
-students <- dbReadTable(con, "Students")
-departments <- dbReadTable(con, "Departments")
-courses <- dbReadTable(con, "Courses")
-
-## Joins
-classroom_courses <- inner_join(classroom, courses, by="CourseID")
-class_courses_dep <- inner_join(departments ,classroom_courses, by="DepartmentID")
-class_courses_dep_stud <- inner_join(class_courses_dep, students, by="StudentID")
-class_courses_dep_stud_teach <- inner_join(class_courses_dep_stud, teachers, by="TeacherID")
-class_courses_dep_teach <- left_join(class_courses_dep, teachers, by="TeacherID")
+df <- dbReadTable(con, "Classrooms")
 
 library(dplyr)
 
@@ -24,42 +19,25 @@ library(dplyr)
 ##############
 ## Q1. Count the number of students on each department¶
 ##############
-students_dep <-class_courses_dep %>% group_by(DepartmentName) %>% 
-                  summarise(No_of_students = n_distinct(StudentID))
-print(students_dep)
+
 
 ##############
 ## Q2. How many students have each course of the English department and the 
 ##     total number of students in the department?
 ##############
-students_eng_dep <- class_courses_dep %>% filter(DepartmentID==1) %>% 
-                      group_by(CourseName) %>% 
-                        summarise(No_of_students = n_distinct(StudentID))
 
-class_courses_dep %>% filter(DepartmentID==1)%>%
-                  summarise(Total = n_distinct(StudentID))
 
 ##############
 ## Q3. How many small (<22 students) and large (22+ students) classrooms are 
 ##     needed for the Science department?
 ##############
 
-students_sci_dep <- class_courses_dep %>% filter(DepartmentID==2) %>% 
-  group_by(CourseName) %>% 
-  summarise(No_of_students = n_distinct(StudentID)) %>% 
-  mutate(Big_Class = ifelse(No_of_students > 21,1,0),
-       Small_Class = ifelse(No_of_students < 23, 1,0)) %>% 
-  summarise(No_Big_Class = sum(Big_Class),
-           No_Small_Class = sum(Small_Class))
 
 ##############
 ## Q4. A feminist student claims that there are more male than female in the 
 ##     College. Justify if the argument is correct
 ##############
-genders <- students  %>% group_by(Gender) %>% 
-  summarise(No_of_students = n_distinct(StudentID))
-genders
-print("The feminist are WRONG, There are more women than man in the college")
+
 
 ##############
 ## Q5. For which courses the percentage of male/female students is over 70%?
