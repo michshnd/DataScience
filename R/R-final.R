725f821d12764bd8f915b7eb9b480513ee259802
“michshnd” <michshnd@gmail.com>
2019-11-30 15:07:36 +0200

    R HW ex.7

--- R/R-final.R
+++ R/R-final.R
@@ -65,16 +65,34 @@ print("The feminist are WRONG, There are more women than man in the college")
 ## Q5. For which courses the percentage of male/female students is over 70%?
 ##############
 
+Male_Female <-  class_courses_dep_stud %>% mutate(Male = ifelse(Gender=="M",1,0),
+                  Female = ifelse(Gender=="F",1,0)) %>%group_by(CourseID,CourseName) %>% 
+                  summarise(Male = sum(Male),Female = sum(Female),Total = n()) %>%
+                   filter((Male/Total>0.7) || (Female/Total>0.7))
+                  
+Male_Female                 
+
+
 
 ##############
 ## Q6. For each department, how many students passed with a grades over 80?
 ##############
 
+depart_stud_80 <-  class_courses_dep_stud %>% filter(degree >=80) %>% 
+                    group_by(DepartmentID,DepartmentName) %>% summarise(Over_80 = n_distinct(StudentID))
+               
+depart_stud_80
+  
 
 ##############
 ## Q7. For each department, how many students passed with a grades under 60?
 ##############
 
+
+depart_stud_60 <-  class_courses_dep_stud %>% filter(degree <=60) %>% 
+  group_by(DepartmentID,DepartmentName) %>% summarise(under_60 = n_distinct(StudentID))
+
+depart_stud_60
 
 ##############
 ## Q8. Rate the teachers by their average student's grades (in descending order).
